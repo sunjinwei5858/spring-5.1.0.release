83c106ea91bbf694e51e8a2aae4ad90c1757675b
sunjinwei <sunjinwei5828@163.com>
2020-10-09 07:01:43 +0800

    父子通过HierarchicalBeanFactory该接口进行关联

--- spring-beans/src/main/java/org/springframework/beans/factory/HierarchicalBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/HierarchicalBeanFactory.java
@@ -19,6 +19,8 @@ package org.springframework.beans.factory;
 import org.springframework.lang.Nullable;
 
 /**
+ * 父子容器通过该接口进行关联
+ * <p>
  * Sub-interface implemented by bean factories that can be part
  * of a hierarchy.
  *
@@ -28,26 +30,27 @@ import org.springframework.lang.Nullable;
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
- * @since 07.07.2003
  * @see org.springframework.beans.factory.config.ConfigurableBeanFactory#setParentBeanFactory
+ * @since 07.07.2003
  */
 public interface HierarchicalBeanFactory extends BeanFactory {
 
-	/**
-	 * Return the parent bean factory, or {@code null} if there is none.
-	 */
-	@Nullable
-	BeanFactory getParentBeanFactory();
+    /**
+     * Return the parent bean factory, or {@code null} if there is none.
+     */
+    @Nullable
+    BeanFactory getParentBeanFactory();
 
-	/**
-	 * Return whether the local bean factory contains a bean of the given name,
-	 * ignoring beans defined in ancestor contexts.
-	 * <p>This is an alternative to {@code containsBean}, ignoring a bean
-	 * of the given name from an ancestor bean factory.
-	 * @param name the name of the bean to query
-	 * @return whether a bean with the given name is defined in the local factory
-	 * @see BeanFactory#containsBean
-	 */
-	boolean containsLocalBean(String name);
+    /**
+     * Return whether the local bean factory contains a bean of the given name,
+     * ignoring beans defined in ancestor contexts.
+     * <p>This is an alternative to {@code containsBean}, ignoring a bean
+     * of the given name from an ancestor bean factory.
+     *
+     * @param name the name of the bean to query
+     * @return whether a bean with the given name is defined in the local factory
+     * @see BeanFactory#containsBean
+     */
+    boolean containsLocalBean(String name);
 
 }
