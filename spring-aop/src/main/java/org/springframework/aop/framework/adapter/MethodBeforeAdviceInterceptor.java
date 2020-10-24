6d18d237a2c48bbb211f2b82ddde18a094871330
sunjinwei <sunjinwei5828@163.com>
2020-10-24 17:47:14 +0800

    MethodBeforeAdviceInterceptor前置通知拦截器，真正调用前置通知方法的地方

--- spring-aop/src/main/java/org/springframework/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
@@ -16,16 +16,17 @@
 
 package org.springframework.aop.framework.adapter;
 
-import java.io.Serializable;
-
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.util.Assert;
 
+import java.io.Serializable;
+
 /**
+ * 前置通知拦截器，真正调用before方法的地方!!!
+ *
  * Interceptor to wrap am {@link org.springframework.aop.MethodBeforeAdvice}.
  * Used internally by the AOP framework; application developers should not need
  * to use this class directly.
@@ -37,23 +38,24 @@ import org.springframework.util.Assert;
 @SuppressWarnings("serial")
 public class MethodBeforeAdviceInterceptor implements MethodInterceptor, BeforeAdvice, Serializable {
 
-	private final MethodBeforeAdvice advice;
+    private final MethodBeforeAdvice advice;
 
 
-	/**
-	 * Create a new MethodBeforeAdviceInterceptor for the given advice.
-	 * @param advice the MethodBeforeAdvice to wrap
-	 */
-	public MethodBeforeAdviceInterceptor(MethodBeforeAdvice advice) {
-		Assert.notNull(advice, "Advice must not be null");
-		this.advice = advice;
-	}
+    /**
+     * Create a new MethodBeforeAdviceInterceptor for the given advice.
+     *
+     * @param advice the MethodBeforeAdvice to wrap
+     */
+    public MethodBeforeAdviceInterceptor(MethodBeforeAdvice advice) {
+        Assert.notNull(advice, "Advice must not be null");
+        this.advice = advice;
+    }
 
 
-	@Override
-	public Object invoke(MethodInvocation mi) throws Throwable {
-		this.advice.before(mi.getMethod(), mi.getArguments(), mi.getThis());
-		return mi.proceed();
-	}
+    @Override
+    public Object invoke(MethodInvocation mi) throws Throwable {
+        this.advice.before(mi.getMethod(), mi.getArguments(), mi.getThis());
+        return mi.proceed();
+    }
 
 }
