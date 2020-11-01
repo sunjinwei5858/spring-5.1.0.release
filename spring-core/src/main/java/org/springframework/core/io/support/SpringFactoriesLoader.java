1cac6687e5090072dd739a8de4e043f542d07145
sunjinwei <sunjinwei5828@163.com>
2020-11-01 15:44:44 +0800

    spring的spi机制，加载spring.factories文件的spi接口实现类。第一次全部加载，第二次就从缓存中取!!!!!!

--- spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
+++ spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
@@ -16,31 +16,22 @@
 
 package org.springframework.core.io.support;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.io.UrlResource;
 import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.ConcurrentReferenceHashMap;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.util.StringUtils;
+import org.springframework.util.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 /**
+ * spring的spi机制：
+ * 1。spring内部维护了一个容器，其中key为类加载器，value为
+ * <p>
+ * <p>
  * General purpose factory loading mechanism for internal use within the framework.
  *
  * <p>{@code SpringFactoriesLoader} {@linkplain #loadFactories loads} and instantiates
@@ -51,7 +42,7 @@ import org.springframework.util.StringUtils;
  * implementation class names. For example:
  *
  * <pre class="code">example.MyService=example.MyServiceImpl1,example.MyServiceImpl2</pre>
- *
+ * <p>
  * where {@code example.MyService} is the name of the interface, and {@code MyServiceImpl1}
  * and {@code MyServiceImpl2} are two implementations.
  *
@@ -62,110 +53,129 @@ import org.springframework.util.StringUtils;
  */
 public final class SpringFactoriesLoader {
 
-	/**
-	 * The location to look for factories.
-	 * <p>Can be present in multiple JAR files.
-	 */
-	public static final String FACTORIES_RESOURCE_LOCATION = "META-INF/spring.factories";
+    /**
+     * The location to look for factories.
+     * <p>Can be present in multiple JAR files.
+     */
+    public static final String FACTORIES_RESOURCE_LOCATION = "META-INF/spring.factories";
 
 
-	private static final Log logger = LogFactory.getLog(SpringFactoriesLoader.class);
+    private static final Log logger = LogFactory.getLog(SpringFactoriesLoader.class);
 
-	private static final Map<ClassLoader, MultiValueMap<String, String>> cache = new ConcurrentReferenceHashMap<>();
+    /**
+     * spi机制：key为类加载器 value为spring.factories里面的spi接口和对应的实现类
+     */
+    private static final Map<ClassLoader, MultiValueMap<String, String>> cache = new ConcurrentReferenceHashMap<>();
 
 
-	private SpringFactoriesLoader() {
-	}
+    private SpringFactoriesLoader() {
+    }
 
 
-	/**
-	 * Load and instantiate the factory implementations of the given type from
-	 * {@value #FACTORIES_RESOURCE_LOCATION}, using the given class loader.
-	 * <p>The returned factories are sorted through {@link AnnotationAwareOrderComparator}.
-	 * <p>If a custom instantiation strategy is required, use {@link #loadFactoryNames}
-	 * to obtain all registered factory names.
-	 * @param factoryClass the interface or abstract class representing the factory
-	 * @param classLoader the ClassLoader to use for loading (can be {@code null} to use the default)
-	 * @throws IllegalArgumentException if any factory implementation class cannot
-	 * be loaded or if an error occurs while instantiating any factory
-	 * @see #loadFactoryNames
-	 */
-	public static <T> List<T> loadFactories(Class<T> factoryClass, @Nullable ClassLoader classLoader) {
-		Assert.notNull(factoryClass, "'factoryClass' must not be null");
-		ClassLoader classLoaderToUse = classLoader;
-		if (classLoaderToUse == null) {
-			classLoaderToUse = SpringFactoriesLoader.class.getClassLoader();
-		}
-		List<String> factoryNames = loadFactoryNames(factoryClass, classLoaderToUse);
-		if (logger.isTraceEnabled()) {
-			logger.trace("Loaded [" + factoryClass.getName() + "] names: " + factoryNames);
-		}
-		List<T> result = new ArrayList<>(factoryNames.size());
-		for (String factoryName : factoryNames) {
-			result.add(instantiateFactory(factoryName, factoryClass, classLoaderToUse));
-		}
-		AnnotationAwareOrderComparator.sort(result);
-		return result;
-	}
+    /**
+     * 注意：
+     * 1。如果不传入类加载器，那么spring直接根据class的类加载器去加载
+     * 2。如果指定了类加载器，那么就根据指定的类加载器去加载，springboot就是传入了classLoader【默认类加载器--线程上下文类加载器】
+     * <p>
+     * Load and instantiate the factory implementations of the given type from
+     * {@value #FACTORIES_RESOURCE_LOCATION}, using the given class loader.
+     * <p>The returned factories are sorted through {@link AnnotationAwareOrderComparator}.
+     * <p>If a custom instantiation strategy is required, use {@link #loadFactoryNames}
+     * to obtain all registered factory names.
+     *
+     * @param factoryClass the interface or abstract class representing the factory
+     * @param classLoader  the ClassLoader to use for loading (can be {@code null} to use the default)
+     * @throws IllegalArgumentException if any factory implementation class cannot
+     *                                  be loaded or if an error occurs while instantiating any factory
+     * @see #loadFactoryNames
+     */
+    public static <T> List<T> loadFactories(Class<T> factoryClass, @Nullable ClassLoader classLoader) {
+        Assert.notNull(factoryClass, "'factoryClass' must not be null");
+        ClassLoader classLoaderToUse = classLoader;
+        /**
+         * 如果参数没有指定classLoader 那么
+         */
+        if (classLoaderToUse == null) {
+            classLoaderToUse = SpringFactoriesLoader.class.getClassLoader();
+        }
+        List<String> factoryNames = loadFactoryNames(factoryClass, classLoaderToUse);
+        if (logger.isTraceEnabled()) {
+            logger.trace("Loaded [" + factoryClass.getName() + "] names: " + factoryNames);
+        }
+        List<T> result = new ArrayList<>(factoryNames.size());
+        for (String factoryName : factoryNames) {
+            result.add(instantiateFactory(factoryName, factoryClass, classLoaderToUse));
+        }
+        AnnotationAwareOrderComparator.sort(result);
+        return result;
+    }
 
-	/**
-	 * Load the fully qualified class names of factory implementations of the
-	 * given type from {@value #FACTORIES_RESOURCE_LOCATION}, using the given
-	 * class loader.
-	 * @param factoryClass the interface or abstract class representing the factory
-	 * @param classLoader the ClassLoader to use for loading resources; can be
-	 * {@code null} to use the default
-	 * @throws IllegalArgumentException if an error occurs while loading factory names
-	 * @see #loadFactories
-	 */
-	public static List<String> loadFactoryNames(Class<?> factoryClass, @Nullable ClassLoader classLoader) {
-		String factoryClassName = factoryClass.getName();
-		return loadSpringFactories(classLoader).getOrDefault(factoryClassName, Collections.emptyList());
-	}
+    /**
+     * spi机制大法：
+     * springboot传入了默认的类加载器【线程上下文】，直接调用了loadFactoryNames这个静态方法获取spi接口实现类的全限定名集合
+     * <p>
+     * Load the fully qualified class names of factory implementations of the
+     * given type from {@value #FACTORIES_RESOURCE_LOCATION}, using the given
+     * class loader.
+     *
+     * @param factoryClass the interface or abstract class representing the factory
+     * @param classLoader  the ClassLoader to use for loading resources; can be
+     *                     {@code null} to use the default
+     * @throws IllegalArgumentException if an error occurs while loading factory names
+     * @see #loadFactories
+     */
+    public static List<String> loadFactoryNames(Class<?> factoryClass, @Nullable ClassLoader classLoader) {
+        String factoryClassName = factoryClass.getName();
+        return loadSpringFactories(classLoader).getOrDefault(factoryClassName, Collections.emptyList());
+    }
 
-	private static Map<String, List<String>> loadSpringFactories(@Nullable ClassLoader classLoader) {
-		MultiValueMap<String, String> result = cache.get(classLoader);
-		if (result != null) {
-			return result;
-		}
+    private static Map<String, List<String>> loadSpringFactories(@Nullable ClassLoader classLoader) {
 
-		try {
-			Enumeration<URL> urls = (classLoader != null ?
-					classLoader.getResources(FACTORIES_RESOURCE_LOCATION) :
-					ClassLoader.getSystemResources(FACTORIES_RESOURCE_LOCATION));
-			result = new LinkedMultiValueMap<>();
-			while (urls.hasMoreElements()) {
-				URL url = urls.nextElement();
-				UrlResource resource = new UrlResource(url);
-				Properties properties = PropertiesLoaderUtils.loadProperties(resource);
-				for (Map.Entry<?, ?> entry : properties.entrySet()) {
-					List<String> factoryClassNames = Arrays.asList(
-							StringUtils.commaDelimitedListToStringArray((String) entry.getValue()));
-					result.addAll((String) entry.getKey(), factoryClassNames);
-				}
-			}
-			cache.put(classLoader, result);
-			return result;
-		}
-		catch (IOException ex) {
-			throw new IllegalArgumentException("Unable to load factories from location [" +
-					FACTORIES_RESOURCE_LOCATION + "]", ex);
-		}
-	}
+        /**
+         * classLoader作为key，先从缓存中取
+         */
+        MultiValueMap<String, String> result = cache.get(classLoader);
 
-	@SuppressWarnings("unchecked")
-	private static <T> T instantiateFactory(String instanceClassName, Class<T> factoryClass, ClassLoader classLoader) {
-		try {
-			Class<?> instanceClass = ClassUtils.forName(instanceClassName, classLoader);
-			if (!factoryClass.isAssignableFrom(instanceClass)) {
-				throw new IllegalArgumentException(
-						"Class [" + instanceClassName + "] is not assignable to [" + factoryClass.getName() + "]");
-			}
-			return (T) ReflectionUtils.accessibleConstructor(instanceClass).newInstance();
-		}
-		catch (Throwable ex) {
-			throw new IllegalArgumentException("Unable to instantiate factory class: " + factoryClass.getName(), ex);
-		}
-	}
+        if (result != null) {
+            return result;
+        }
+
+        try {
+            Enumeration<URL> urls = (classLoader != null ? classLoader.getResources(FACTORIES_RESOURCE_LOCATION) : ClassLoader.getSystemResources(FACTORIES_RESOURCE_LOCATION));
+            result = new LinkedMultiValueMap<>();
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                UrlResource resource = new UrlResource(url);
+                Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+                for (Map.Entry<?, ?> entry : properties.entrySet()) {
+                    String[] stringArray = StringUtils.commaDelimitedListToStringArray((String) entry.getValue());
+                    List<String> factoryClassNames = Arrays.asList(stringArray);
+                    result.addAll((String) entry.getKey(), factoryClassNames);
+                }
+            }
+            /**
+             * 加入缓存
+             */
+            cache.put(classLoader, result);
+            return result;
+        } catch (IOException ex) {
+            throw new IllegalArgumentException("Unable to load factories from location [" +
+                    FACTORIES_RESOURCE_LOCATION + "]", ex);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T instantiateFactory(String instanceClassName, Class<T> factoryClass, ClassLoader classLoader) {
+        try {
+            Class<?> instanceClass = ClassUtils.forName(instanceClassName, classLoader);
+            if (!factoryClass.isAssignableFrom(instanceClass)) {
+                throw new IllegalArgumentException(
+                        "Class [" + instanceClassName + "] is not assignable to [" + factoryClass.getName() + "]");
+            }
+            return (T) ReflectionUtils.accessibleConstructor(instanceClass).newInstance();
+        } catch (Throwable ex) {
+            throw new IllegalArgumentException("Unable to instantiate factory class: " + factoryClass.getName(), ex);
+        }
+    }
 
 }
