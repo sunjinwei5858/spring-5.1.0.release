a2f30d489b9f07b78705febb85d4c1e6fdfea53e
sunjinwei <sunjinwei5828@163.com>
2020-11-04 19:00:13 +0800

    TransactionInterceptor事务增强器，继承了TransactionAspectSupport父类

--- spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionInterceptor.java
+++ spring-tx/src/main/java/org/springframework/transaction/interceptor/TransactionInterceptor.java
@@ -16,21 +16,23 @@
 
 package org.springframework.transaction.interceptor;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.transaction.PlatformTransactionManager;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Properties;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-
-import org.springframework.aop.support.AopUtils;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.lang.Nullable;
-import org.springframework.transaction.PlatformTransactionManager;
-
 /**
+ * 事务增强器：支持整个事务功能的架构，该拦截器是如何实现事务特性的
+ * <p>
+ * <p>
  * AOP Alliance MethodInterceptor for declarative transaction
  * management using the common Spring transaction infrastructure
  * ({@link org.springframework.transaction.PlatformTransactionManager}).
@@ -51,80 +53,93 @@ import org.springframework.transaction.PlatformTransactionManager;
 @SuppressWarnings("serial")
 public class TransactionInterceptor extends TransactionAspectSupport implements MethodInterceptor, Serializable {
 
-	/**
-	 * Create a new TransactionInterceptor.
-	 * <p>Transaction manager and transaction attributes still need to be set.
-	 * @see #setTransactionManager
-	 * @see #setTransactionAttributes(java.util.Properties)
-	 * @see #setTransactionAttributeSource(TransactionAttributeSource)
-	 */
-	public TransactionInterceptor() {
-	}
+    /**
+     * Create a new TransactionInterceptor.
+     * <p>Transaction manager and transaction attributes still need to be set.
+     *
+     * @see #setTransactionManager
+     * @see #setTransactionAttributes(java.util.Properties)
+     * @see #setTransactionAttributeSource(TransactionAttributeSource)
+     */
+    public TransactionInterceptor() {
+    }
 
-	/**
-	 * Create a new TransactionInterceptor.
-	 * @param ptm the default transaction manager to perform the actual transaction management
-	 * @param attributes the transaction attributes in properties format
-	 * @see #setTransactionManager
-	 * @see #setTransactionAttributes(java.util.Properties)
-	 */
-	public TransactionInterceptor(PlatformTransactionManager ptm, Properties attributes) {
-		setTransactionManager(ptm);
-		setTransactionAttributes(attributes);
-	}
+    /**
+     * Create a new TransactionInterceptor.
+     *
+     * @param ptm        the default transaction manager to perform the actual transaction management
+     * @param attributes the transaction attributes in properties format
+     * @see #setTransactionManager
+     * @see #setTransactionAttributes(java.util.Properties)
+     */
+    public TransactionInterceptor(PlatformTransactionManager ptm, Properties attributes) {
+        setTransactionManager(ptm);
+        setTransactionAttributes(attributes);
+    }
 
-	/**
-	 * Create a new TransactionInterceptor.
-	 * @param ptm the default transaction manager to perform the actual transaction management
-	 * @param tas the attribute source to be used to find transaction attributes
-	 * @see #setTransactionManager
-	 * @see #setTransactionAttributeSource(TransactionAttributeSource)
-	 */
-	public TransactionInterceptor(PlatformTransactionManager ptm, TransactionAttributeSource tas) {
-		setTransactionManager(ptm);
-		setTransactionAttributeSource(tas);
-	}
-
-
-	@Override
-	@Nullable
-	public Object invoke(MethodInvocation invocation) throws Throwable {
-		// Work out the target class: may be {@code null}.
-		// The TransactionAttributeSource should be passed the target class
-		// as well as the method, which may be from an interface.
-		Class<?> targetClass = (invocation.getThis() != null ? AopUtils.getTargetClass(invocation.getThis()) : null);
-
-		// Adapt to TransactionAspectSupport's invokeWithinTransaction...
-		return invokeWithinTransaction(invocation.getMethod(), targetClass, invocation::proceed);
-	}
+    /**
+     * Create a new TransactionInterceptor.
+     *
+     * @param ptm the default transaction manager to perform the actual transaction management
+     * @param tas the attribute source to be used to find transaction attributes
+     * @see #setTransactionManager
+     * @see #setTransactionAttributeSource(TransactionAttributeSource)
+     */
+    public TransactionInterceptor(PlatformTransactionManager ptm, TransactionAttributeSource tas) {
+        setTransactionManager(ptm);
+        setTransactionAttributeSource(tas);
+    }
 
 
-	//---------------------------------------------------------------------
-	// Serialization support
-	//---------------------------------------------------------------------
+    /**
+     * 调用invoke的源头，其实会调用父类的invokeWithinTransaction方法，才是真正执行的方法
+     *
+     * @param invocation the method invocation joinpoint
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    @Nullable
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        // Work out the target class: may be {@code null}.
+        // The TransactionAttributeSource should be passed the target class
+        // as well as the method, which may be from an interface.
+        Class<?> targetClass = (invocation.getThis() != null ? AopUtils.getTargetClass(invocation.getThis()) : null);
 
-	private void writeObject(ObjectOutputStream oos) throws IOException {
-		// Rely on default serialization, although this class itself doesn't carry state anyway...
-		oos.defaultWriteObject();
+        // Adapt to TransactionAspectSupport's invokeWithinTransaction...
+        /**
+         * 调用父类TransactionAspectSupport的invokeWithinTransaction方法
+         */
+        return invokeWithinTransaction(invocation.getMethod(), targetClass, invocation::proceed);
+    }
 
-		// Deserialize superclass fields.
-		oos.writeObject(getTransactionManagerBeanName());
-		oos.writeObject(getTransactionManager());
-		oos.writeObject(getTransactionAttributeSource());
-		oos.writeObject(getBeanFactory());
-	}
 
-	private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-		// Rely on default serialization, although this class itself doesn't carry state anyway...
-		ois.defaultReadObject();
+    //---------------------------------------------------------------------
+    // Serialization support
+    //---------------------------------------------------------------------
 
-		// Serialize all relevant superclass fields.
-		// Superclass can't implement Serializable because it also serves as base class
-		// for AspectJ aspects (which are not allowed to implement Serializable)!
-		setTransactionManagerBeanName((String) ois.readObject());
-		setTransactionManager((PlatformTransactionManager) ois.readObject());
-		setTransactionAttributeSource((TransactionAttributeSource) ois.readObject());
-		setBeanFactory((BeanFactory) ois.readObject());
-	}
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        // Rely on default serialization, although this class itself doesn't carry state anyway...
+        oos.defaultWriteObject();
+
+        // Deserialize superclass fields.
+        oos.writeObject(getTransactionManagerBeanName());
+        oos.writeObject(getTransactionManager());
+        oos.writeObject(getTransactionAttributeSource());
+        oos.writeObject(getBeanFactory());
+    }
+
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        // Rely on default serialization, although this class itself doesn't carry state anyway...
+        ois.defaultReadObject();
+
+        // Serialize all relevant superclass fields.
+        // Superclass can't implement Serializable because it also serves as base class
+        // for AspectJ aspects (which are not allowed to implement Serializable)!
+        setTransactionManagerBeanName((String) ois.readObject());
+        setTransactionManager((PlatformTransactionManager) ois.readObject());
+        setTransactionAttributeSource((TransactionAttributeSource) ois.readObject());
+        setBeanFactory((BeanFactory) ois.readObject());
+    }
 
 }
