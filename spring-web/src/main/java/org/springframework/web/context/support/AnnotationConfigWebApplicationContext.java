29efd52018f8806461701fb0e13a9a86634863f4
sunjinwei <sunjinwei5828@163.com>
2020-11-23 12:53:44 +0800

    spring-web容器的实现类：AbstractRefreshableWebApplicationContext,XmlWebApplicationContext,AnnotationConfigWebApplicationContext

--- spring-web/src/main/java/org/springframework/web/context/support/AnnotationConfigWebApplicationContext.java
+++ spring-web/src/main/java/org/springframework/web/context/support/AnnotationConfigWebApplicationContext.java
@@ -16,24 +16,22 @@
 
 package org.springframework.web.context.support;
 
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
-import org.springframework.context.annotation.AnnotationConfigRegistry;
-import org.springframework.context.annotation.AnnotationConfigUtils;
-import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
-import org.springframework.context.annotation.ScopeMetadataResolver;
+import org.springframework.context.annotation.*;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.ContextLoader;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
+ * servlet3.0通过spi机制初始化的web容器类型：AnnotationConfigWebApplicationContext
+ * <p>
  * {@link org.springframework.web.context.WebApplicationContext WebApplicationContext}
  * implementation which accepts annotated classes as input - in particular
  * {@link org.springframework.context.annotation.Configuration @Configuration}-annotated
@@ -78,203 +76,209 @@ import org.springframework.web.context.ContextLoader;
  *
  * @author Chris Beams
  * @author Juergen Hoeller
- * @since 3.0
  * @see org.springframework.context.annotation.AnnotationConfigApplicationContext
+ * @since 3.0
  */
 public class AnnotationConfigWebApplicationContext extends AbstractRefreshableWebApplicationContext
-		implements AnnotationConfigRegistry {
+        implements AnnotationConfigRegistry {
 
-	@Nullable
-	private BeanNameGenerator beanNameGenerator;
+    @Nullable
+    private BeanNameGenerator beanNameGenerator;
 
-	@Nullable
-	private ScopeMetadataResolver scopeMetadataResolver;
+    @Nullable
+    private ScopeMetadataResolver scopeMetadataResolver;
 
-	private final Set<Class<?>> annotatedClasses = new LinkedHashSet<>();
+    private final Set<Class<?>> annotatedClasses = new LinkedHashSet<>();
 
-	private final Set<String> basePackages = new LinkedHashSet<>();
-
-
-	/**
-	 * Set a custom {@link BeanNameGenerator} for use with {@link AnnotatedBeanDefinitionReader}
-	 * and/or {@link ClassPathBeanDefinitionScanner}.
-	 * <p>Default is {@link org.springframework.context.annotation.AnnotationBeanNameGenerator}.
-	 * @see AnnotatedBeanDefinitionReader#setBeanNameGenerator
-	 * @see ClassPathBeanDefinitionScanner#setBeanNameGenerator
-	 */
-	public void setBeanNameGenerator(@Nullable BeanNameGenerator beanNameGenerator) {
-		this.beanNameGenerator = beanNameGenerator;
-	}
-
-	/**
-	 * Return the custom {@link BeanNameGenerator} for use with {@link AnnotatedBeanDefinitionReader}
-	 * and/or {@link ClassPathBeanDefinitionScanner}, if any.
-	 */
-	@Nullable
-	protected BeanNameGenerator getBeanNameGenerator() {
-		return this.beanNameGenerator;
-	}
-
-	/**
-	 * Set a custom {@link ScopeMetadataResolver} for use with {@link AnnotatedBeanDefinitionReader}
-	 * and/or {@link ClassPathBeanDefinitionScanner}.
-	 * <p>Default is an {@link org.springframework.context.annotation.AnnotationScopeMetadataResolver}.
-	 * @see AnnotatedBeanDefinitionReader#setScopeMetadataResolver
-	 * @see ClassPathBeanDefinitionScanner#setScopeMetadataResolver
-	 */
-	public void setScopeMetadataResolver(@Nullable ScopeMetadataResolver scopeMetadataResolver) {
-		this.scopeMetadataResolver = scopeMetadataResolver;
-	}
-
-	/**
-	 * Return the custom {@link ScopeMetadataResolver} for use with {@link AnnotatedBeanDefinitionReader}
-	 * and/or {@link ClassPathBeanDefinitionScanner}, if any.
-	 */
-	@Nullable
-	protected ScopeMetadataResolver getScopeMetadataResolver() {
-		return this.scopeMetadataResolver;
-	}
+    private final Set<String> basePackages = new LinkedHashSet<>();
 
 
-	/**
-	 * Register one or more annotated classes to be processed.
-	 * <p>Note that {@link #refresh()} must be called in order for the context
-	 * to fully process the new classes.
-	 * @param annotatedClasses one or more annotated classes,
-	 * e.g. {@link org.springframework.context.annotation.Configuration @Configuration} classes
-	 * @see #scan(String...)
-	 * @see #loadBeanDefinitions(DefaultListableBeanFactory)
-	 * @see #setConfigLocation(String)
-	 * @see #refresh()
-	 */
-	public void register(Class<?>... annotatedClasses) {
-		Assert.notEmpty(annotatedClasses, "At least one annotated class must be specified");
-		Collections.addAll(this.annotatedClasses, annotatedClasses);
-	}
+    /**
+     * Set a custom {@link BeanNameGenerator} for use with {@link AnnotatedBeanDefinitionReader}
+     * and/or {@link ClassPathBeanDefinitionScanner}.
+     * <p>Default is {@link org.springframework.context.annotation.AnnotationBeanNameGenerator}.
+     *
+     * @see AnnotatedBeanDefinitionReader#setBeanNameGenerator
+     * @see ClassPathBeanDefinitionScanner#setBeanNameGenerator
+     */
+    public void setBeanNameGenerator(@Nullable BeanNameGenerator beanNameGenerator) {
+        this.beanNameGenerator = beanNameGenerator;
+    }
 
-	/**
-	 * Perform a scan within the specified base packages.
-	 * <p>Note that {@link #refresh()} must be called in order for the context
-	 * to fully process the new classes.
-	 * @param basePackages the packages to check for annotated classes
-	 * @see #loadBeanDefinitions(DefaultListableBeanFactory)
-	 * @see #register(Class...)
-	 * @see #setConfigLocation(String)
-	 * @see #refresh()
-	 */
-	public void scan(String... basePackages) {
-		Assert.notEmpty(basePackages, "At least one base package must be specified");
-		Collections.addAll(this.basePackages, basePackages);
-	}
+    /**
+     * Return the custom {@link BeanNameGenerator} for use with {@link AnnotatedBeanDefinitionReader}
+     * and/or {@link ClassPathBeanDefinitionScanner}, if any.
+     */
+    @Nullable
+    protected BeanNameGenerator getBeanNameGenerator() {
+        return this.beanNameGenerator;
+    }
 
+    /**
+     * Set a custom {@link ScopeMetadataResolver} for use with {@link AnnotatedBeanDefinitionReader}
+     * and/or {@link ClassPathBeanDefinitionScanner}.
+     * <p>Default is an {@link org.springframework.context.annotation.AnnotationScopeMetadataResolver}.
+     *
+     * @see AnnotatedBeanDefinitionReader#setScopeMetadataResolver
+     * @see ClassPathBeanDefinitionScanner#setScopeMetadataResolver
+     */
+    public void setScopeMetadataResolver(@Nullable ScopeMetadataResolver scopeMetadataResolver) {
+        this.scopeMetadataResolver = scopeMetadataResolver;
+    }
 
-	/**
-	 * Register a {@link org.springframework.beans.factory.config.BeanDefinition} for
-	 * any classes specified by {@link #register(Class...)} and scan any packages
-	 * specified by {@link #scan(String...)}.
-	 * <p>For any values specified by {@link #setConfigLocation(String)} or
-	 * {@link #setConfigLocations(String[])}, attempt first to load each location as a
-	 * class, registering a {@code BeanDefinition} if class loading is successful,
-	 * and if class loading fails (i.e. a {@code ClassNotFoundException} is raised),
-	 * assume the value is a package and attempt to scan it for annotated classes.
-	 * <p>Enables the default set of annotation configuration post processors, such that
-	 * {@code @Autowired}, {@code @Required}, and associated annotations can be used.
-	 * <p>Configuration class bean definitions are registered with generated bean
-	 * definition names unless the {@code value} attribute is provided to the stereotype
-	 * annotation.
-	 * @param beanFactory the bean factory to load bean definitions into
-	 * @see #register(Class...)
-	 * @see #scan(String...)
-	 * @see #setConfigLocation(String)
-	 * @see #setConfigLocations(String[])
-	 * @see AnnotatedBeanDefinitionReader
-	 * @see ClassPathBeanDefinitionScanner
-	 */
-	@Override
-	protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) {
-		AnnotatedBeanDefinitionReader reader = getAnnotatedBeanDefinitionReader(beanFactory);
-		ClassPathBeanDefinitionScanner scanner = getClassPathBeanDefinitionScanner(beanFactory);
-
-		BeanNameGenerator beanNameGenerator = getBeanNameGenerator();
-		if (beanNameGenerator != null) {
-			reader.setBeanNameGenerator(beanNameGenerator);
-			scanner.setBeanNameGenerator(beanNameGenerator);
-			beanFactory.registerSingleton(AnnotationConfigUtils.CONFIGURATION_BEAN_NAME_GENERATOR, beanNameGenerator);
-		}
-
-		ScopeMetadataResolver scopeMetadataResolver = getScopeMetadataResolver();
-		if (scopeMetadataResolver != null) {
-			reader.setScopeMetadataResolver(scopeMetadataResolver);
-			scanner.setScopeMetadataResolver(scopeMetadataResolver);
-		}
-
-		if (!this.annotatedClasses.isEmpty()) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("Registering annotated classes: [" +
-						StringUtils.collectionToCommaDelimitedString(this.annotatedClasses) + "]");
-			}
-			reader.register(ClassUtils.toClassArray(this.annotatedClasses));
-		}
-
-		if (!this.basePackages.isEmpty()) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("Scanning base packages: [" +
-						StringUtils.collectionToCommaDelimitedString(this.basePackages) + "]");
-			}
-			scanner.scan(StringUtils.toStringArray(this.basePackages));
-		}
-
-		String[] configLocations = getConfigLocations();
-		if (configLocations != null) {
-			for (String configLocation : configLocations) {
-				try {
-					Class<?> clazz = ClassUtils.forName(configLocation, getClassLoader());
-					if (logger.isTraceEnabled()) {
-						logger.trace("Registering [" + configLocation + "]");
-					}
-					reader.register(clazz);
-				}
-				catch (ClassNotFoundException ex) {
-					if (logger.isTraceEnabled()) {
-						logger.trace("Could not load class for config location [" + configLocation +
-								"] - trying package scan. " + ex);
-					}
-					int count = scanner.scan(configLocation);
-					if (count == 0 && logger.isDebugEnabled()) {
-						logger.debug("No annotated classes found for specified class/package [" + configLocation + "]");
-					}
-				}
-			}
-		}
-	}
+    /**
+     * Return the custom {@link ScopeMetadataResolver} for use with {@link AnnotatedBeanDefinitionReader}
+     * and/or {@link ClassPathBeanDefinitionScanner}, if any.
+     */
+    @Nullable
+    protected ScopeMetadataResolver getScopeMetadataResolver() {
+        return this.scopeMetadataResolver;
+    }
 
 
-	/**
-	 * Build an {@link AnnotatedBeanDefinitionReader} for the given bean factory.
-	 * <p>This should be pre-configured with the {@code Environment} (if desired)
-	 * but not with a {@code BeanNameGenerator} or {@code ScopeMetadataResolver} yet.
-	 * @param beanFactory the bean factory to load bean definitions into
-	 * @since 4.1.9
-	 * @see #getEnvironment()
-	 * @see #getBeanNameGenerator()
-	 * @see #getScopeMetadataResolver()
-	 */
-	protected AnnotatedBeanDefinitionReader getAnnotatedBeanDefinitionReader(DefaultListableBeanFactory beanFactory) {
-		return new AnnotatedBeanDefinitionReader(beanFactory, getEnvironment());
-	}
+    /**
+     * Register one or more annotated classes to be processed.
+     * <p>Note that {@link #refresh()} must be called in order for the context
+     * to fully process the new classes.
+     *
+     * @param annotatedClasses one or more annotated classes,
+     *                         e.g. {@link org.springframework.context.annotation.Configuration @Configuration} classes
+     * @see #scan(String...)
+     * @see #loadBeanDefinitions(DefaultListableBeanFactory)
+     * @see #setConfigLocation(String)
+     * @see #refresh()
+     */
+    public void register(Class<?>... annotatedClasses) {
+        Assert.notEmpty(annotatedClasses, "At least one annotated class must be specified");
+        Collections.addAll(this.annotatedClasses, annotatedClasses);
+    }
 
-	/**
-	 * Build a {@link ClassPathBeanDefinitionScanner} for the given bean factory.
-	 * <p>This should be pre-configured with the {@code Environment} (if desired)
-	 * but not with a {@code BeanNameGenerator} or {@code ScopeMetadataResolver} yet.
-	 * @param beanFactory the bean factory to load bean definitions into
-	 * @since 4.1.9
-	 * @see #getEnvironment()
-	 * @see #getBeanNameGenerator()
-	 * @see #getScopeMetadataResolver()
-	 */
-	protected ClassPathBeanDefinitionScanner getClassPathBeanDefinitionScanner(DefaultListableBeanFactory beanFactory) {
-		return new ClassPathBeanDefinitionScanner(beanFactory, true, getEnvironment());
-	}
+    /**
+     * Perform a scan within the specified base packages.
+     * <p>Note that {@link #refresh()} must be called in order for the context
+     * to fully process the new classes.
+     *
+     * @param basePackages the packages to check for annotated classes
+     * @see #loadBeanDefinitions(DefaultListableBeanFactory)
+     * @see #register(Class...)
+     * @see #setConfigLocation(String)
+     * @see #refresh()
+     */
+    public void scan(String... basePackages) {
+        Assert.notEmpty(basePackages, "At least one base package must be specified");
+        Collections.addAll(this.basePackages, basePackages);
+    }
+
+
+    /**
+     * Register a {@link org.springframework.beans.factory.config.BeanDefinition} for
+     * any classes specified by {@link #register(Class...)} and scan any packages
+     * specified by {@link #scan(String...)}.
+     * <p>For any values specified by {@link #setConfigLocation(String)} or
+     * {@link #setConfigLocations(String[])}, attempt first to load each location as a
+     * class, registering a {@code BeanDefinition} if class loading is successful,
+     * and if class loading fails (i.e. a {@code ClassNotFoundException} is raised),
+     * assume the value is a package and attempt to scan it for annotated classes.
+     * <p>Enables the default set of annotation configuration post processors, such that
+     * {@code @Autowired}, {@code @Required}, and associated annotations can be used.
+     * <p>Configuration class bean definitions are registered with generated bean
+     * definition names unless the {@code value} attribute is provided to the stereotype
+     * annotation.
+     *
+     * @param beanFactory the bean factory to load bean definitions into
+     * @see #register(Class...)
+     * @see #scan(String...)
+     * @see #setConfigLocation(String)
+     * @see #setConfigLocations(String[])
+     * @see AnnotatedBeanDefinitionReader
+     * @see ClassPathBeanDefinitionScanner
+     */
+    @Override
+    protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) {
+        AnnotatedBeanDefinitionReader reader = getAnnotatedBeanDefinitionReader(beanFactory);
+        ClassPathBeanDefinitionScanner scanner = getClassPathBeanDefinitionScanner(beanFactory);
+
+        BeanNameGenerator beanNameGenerator = getBeanNameGenerator();
+        if (beanNameGenerator != null) {
+            reader.setBeanNameGenerator(beanNameGenerator);
+            scanner.setBeanNameGenerator(beanNameGenerator);
+            beanFactory.registerSingleton(AnnotationConfigUtils.CONFIGURATION_BEAN_NAME_GENERATOR, beanNameGenerator);
+        }
+
+        ScopeMetadataResolver scopeMetadataResolver = getScopeMetadataResolver();
+        if (scopeMetadataResolver != null) {
+            reader.setScopeMetadataResolver(scopeMetadataResolver);
+            scanner.setScopeMetadataResolver(scopeMetadataResolver);
+        }
+
+        if (!this.annotatedClasses.isEmpty()) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Registering annotated classes: [" +
+                        StringUtils.collectionToCommaDelimitedString(this.annotatedClasses) + "]");
+            }
+            reader.register(ClassUtils.toClassArray(this.annotatedClasses));
+        }
+
+        if (!this.basePackages.isEmpty()) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Scanning base packages: [" +
+                        StringUtils.collectionToCommaDelimitedString(this.basePackages) + "]");
+            }
+            scanner.scan(StringUtils.toStringArray(this.basePackages));
+        }
+
+        String[] configLocations = getConfigLocations();
+        if (configLocations != null) {
+            for (String configLocation : configLocations) {
+                try {
+                    Class<?> clazz = ClassUtils.forName(configLocation, getClassLoader());
+                    if (logger.isTraceEnabled()) {
+                        logger.trace("Registering [" + configLocation + "]");
+                    }
+                    reader.register(clazz);
+                } catch (ClassNotFoundException ex) {
+                    if (logger.isTraceEnabled()) {
+                        logger.trace("Could not load class for config location [" + configLocation +
+                                "] - trying package scan. " + ex);
+                    }
+                    int count = scanner.scan(configLocation);
+                    if (count == 0 && logger.isDebugEnabled()) {
+                        logger.debug("No annotated classes found for specified class/package [" + configLocation + "]");
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Build an {@link AnnotatedBeanDefinitionReader} for the given bean factory.
+     * <p>This should be pre-configured with the {@code Environment} (if desired)
+     * but not with a {@code BeanNameGenerator} or {@code ScopeMetadataResolver} yet.
+     *
+     * @param beanFactory the bean factory to load bean definitions into
+     * @see #getEnvironment()
+     * @see #getBeanNameGenerator()
+     * @see #getScopeMetadataResolver()
+     * @since 4.1.9
+     */
+    protected AnnotatedBeanDefinitionReader getAnnotatedBeanDefinitionReader(DefaultListableBeanFactory beanFactory) {
+        return new AnnotatedBeanDefinitionReader(beanFactory, getEnvironment());
+    }
+
+    /**
+     * Build a {@link ClassPathBeanDefinitionScanner} for the given bean factory.
+     * <p>This should be pre-configured with the {@code Environment} (if desired)
+     * but not with a {@code BeanNameGenerator} or {@code ScopeMetadataResolver} yet.
+     *
+     * @param beanFactory the bean factory to load bean definitions into
+     * @see #getEnvironment()
+     * @see #getBeanNameGenerator()
+     * @see #getScopeMetadataResolver()
+     * @since 4.1.9
+     */
+    protected ClassPathBeanDefinitionScanner getClassPathBeanDefinitionScanner(DefaultListableBeanFactory beanFactory) {
+        return new ClassPathBeanDefinitionScanner(beanFactory, true, getEnvironment());
+    }
 
 }
--- spring-web/src/main/java/org/springframework/web/context/support/XmlWebApplicationContext.java
+++ spring-web/src/main/java/org/springframework/web/context/support/XmlWebApplicationContext.java
@@ -16,14 +16,16 @@
 
 package org.springframework.web.context.support;
 
-import java.io.IOException;
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.ResourceEntityResolver;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
+import java.io.IOException;
+
 /**
+ * servlet3.0之前通过web.xml的方式【ContextLoadListener-ServletContextListener】,初始化的web容器类型：XmlWebApplicationContext
+ * <p>
  * {@link org.springframework.web.context.WebApplicationContext} implementation
  * which takes its configuration from XML documents, understood by an
  * {@link org.springframework.beans.factory.xml.XmlBeanDefinitionReader}.
@@ -61,85 +63,93 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
  */
 public class XmlWebApplicationContext extends AbstractRefreshableWebApplicationContext {
 
-	/** Default config location for the root context. */
-	public static final String DEFAULT_CONFIG_LOCATION = "/WEB-INF/applicationContext.xml";
+    /**
+     * Default config location for the root context.
+     */
+    public static final String DEFAULT_CONFIG_LOCATION = "/WEB-INF/applicationContext.xml";
 
-	/** Default prefix for building a config location for a namespace. */
-	public static final String DEFAULT_CONFIG_LOCATION_PREFIX = "/WEB-INF/";
+    /**
+     * Default prefix for building a config location for a namespace.
+     */
+    public static final String DEFAULT_CONFIG_LOCATION_PREFIX = "/WEB-INF/";
 
-	/** Default suffix for building a config location for a namespace. */
-	public static final String DEFAULT_CONFIG_LOCATION_SUFFIX = ".xml";
+    /**
+     * Default suffix for building a config location for a namespace.
+     */
+    public static final String DEFAULT_CONFIG_LOCATION_SUFFIX = ".xml";
 
 
-	/**
-	 * Loads the bean definitions via an XmlBeanDefinitionReader.
-	 * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader
-	 * @see #initBeanDefinitionReader
-	 * @see #loadBeanDefinitions
-	 */
-	@Override
-	protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) throws BeansException, IOException {
-		// Create a new XmlBeanDefinitionReader for the given BeanFactory.
-		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+    /**
+     * Loads the bean definitions via an XmlBeanDefinitionReader.
+     *
+     * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+     * @see #initBeanDefinitionReader
+     * @see #loadBeanDefinitions
+     */
+    @Override
+    protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) throws BeansException, IOException {
+        // Create a new XmlBeanDefinitionReader for the given BeanFactory.
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
 
-		// Configure the bean definition reader with this context's
-		// resource loading environment.
-		beanDefinitionReader.setEnvironment(getEnvironment());
-		beanDefinitionReader.setResourceLoader(this);
-		beanDefinitionReader.setEntityResolver(new ResourceEntityResolver(this));
+        // Configure the bean definition reader with this context's
+        // resource loading environment.
+        beanDefinitionReader.setEnvironment(getEnvironment());
+        beanDefinitionReader.setResourceLoader(this);
+        beanDefinitionReader.setEntityResolver(new ResourceEntityResolver(this));
 
-		// Allow a subclass to provide custom initialization of the reader,
-		// then proceed with actually loading the bean definitions.
-		initBeanDefinitionReader(beanDefinitionReader);
-		loadBeanDefinitions(beanDefinitionReader);
-	}
+        // Allow a subclass to provide custom initialization of the reader,
+        // then proceed with actually loading the bean definitions.
+        initBeanDefinitionReader(beanDefinitionReader);
+        loadBeanDefinitions(beanDefinitionReader);
+    }
 
-	/**
-	 * Initialize the bean definition reader used for loading the bean
-	 * definitions of this context. Default implementation is empty.
-	 * <p>Can be overridden in subclasses, e.g. for turning off XML validation
-	 * or using a different XmlBeanDefinitionParser implementation.
-	 * @param beanDefinitionReader the bean definition reader used by this context
-	 * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader#setValidationMode
-	 * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader#setDocumentReaderClass
-	 */
-	protected void initBeanDefinitionReader(XmlBeanDefinitionReader beanDefinitionReader) {
-	}
+    /**
+     * Initialize the bean definition reader used for loading the bean
+     * definitions of this context. Default implementation is empty.
+     * <p>Can be overridden in subclasses, e.g. for turning off XML validation
+     * or using a different XmlBeanDefinitionParser implementation.
+     *
+     * @param beanDefinitionReader the bean definition reader used by this context
+     * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader#setValidationMode
+     * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader#setDocumentReaderClass
+     */
+    protected void initBeanDefinitionReader(XmlBeanDefinitionReader beanDefinitionReader) {
+    }
 
-	/**
-	 * Load the bean definitions with the given XmlBeanDefinitionReader.
-	 * <p>The lifecycle of the bean factory is handled by the refreshBeanFactory method;
-	 * therefore this method is just supposed to load and/or register bean definitions.
-	 * <p>Delegates to a ResourcePatternResolver for resolving location patterns
-	 * into Resource instances.
-	 * @throws IOException if the required XML document isn't found
-	 * @see #refreshBeanFactory
-	 * @see #getConfigLocations
-	 * @see #getResources
-	 * @see #getResourcePatternResolver
-	 */
-	protected void loadBeanDefinitions(XmlBeanDefinitionReader reader) throws IOException {
-		String[] configLocations = getConfigLocations();
-		if (configLocations != null) {
-			for (String configLocation : configLocations) {
-				reader.loadBeanDefinitions(configLocation);
-			}
-		}
-	}
+    /**
+     * Load the bean definitions with the given XmlBeanDefinitionReader.
+     * <p>The lifecycle of the bean factory is handled by the refreshBeanFactory method;
+     * therefore this method is just supposed to load and/or register bean definitions.
+     * <p>Delegates to a ResourcePatternResolver for resolving location patterns
+     * into Resource instances.
+     *
+     * @throws IOException if the required XML document isn't found
+     * @see #refreshBeanFactory
+     * @see #getConfigLocations
+     * @see #getResources
+     * @see #getResourcePatternResolver
+     */
+    protected void loadBeanDefinitions(XmlBeanDefinitionReader reader) throws IOException {
+        String[] configLocations = getConfigLocations();
+        if (configLocations != null) {
+            for (String configLocation : configLocations) {
+                reader.loadBeanDefinitions(configLocation);
+            }
+        }
+    }
 
-	/**
-	 * The default location for the root context is "/WEB-INF/applicationContext.xml",
-	 * and "/WEB-INF/test-servlet.xml" for a context with the namespace "test-servlet"
-	 * (like for a DispatcherServlet instance with the servlet-name "test").
-	 */
-	@Override
-	protected String[] getDefaultConfigLocations() {
-		if (getNamespace() != null) {
-			return new String[] {DEFAULT_CONFIG_LOCATION_PREFIX + getNamespace() + DEFAULT_CONFIG_LOCATION_SUFFIX};
-		}
-		else {
-			return new String[] {DEFAULT_CONFIG_LOCATION};
-		}
-	}
+    /**
+     * The default location for the root context is "/WEB-INF/applicationContext.xml",
+     * and "/WEB-INF/test-servlet.xml" for a context with the namespace "test-servlet"
+     * (like for a DispatcherServlet instance with the servlet-name "test").
+     */
+    @Override
+    protected String[] getDefaultConfigLocations() {
+        if (getNamespace() != null) {
+            return new String[]{DEFAULT_CONFIG_LOCATION_PREFIX + getNamespace() + DEFAULT_CONFIG_LOCATION_SUFFIX};
+        } else {
+            return new String[]{DEFAULT_CONFIG_LOCATION};
+        }
+    }
 
 }
